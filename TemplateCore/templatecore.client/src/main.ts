7daf7910adac3f86545f223af802071e3d33057d
haint0500 <ngotruonghai@vietbank.com.vn>
2024-12-18 10:48:05 +0700

    no message

--- TemplateCore/templatecore.client/src/main.ts
+++
@@ -1,9 +0,0 @@
-import './assets/main.css'
-import router from './router/index.ts'
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-const app =  createApp(App);
-app.mount('#app');
-app.use(router);

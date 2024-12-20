054624e303f9af215d9f7c78e4a56d29fc12570d
haint0500 <ngotruonghai@vietbank.com.vn>
2024-12-20 10:39:01 +0700

    [FE] biến môi trường

--- TemplateCore/templatecore_vuetify/src/App.vue
+++ TemplateCore/templatecore_vuetify/src/App.vue
@@ -7,5 +7,9 @@
 </template>
 
 <script lang="ts" setup>
-  //
+console.log("dsads");
+const apiBaseUrl = import.meta.env.VITE_API_URL;
+
+console.log(`API Base URL: ${apiBaseUrl}`); // https://dev-api.example.com (hoặc production URL)
+
 </script>

12b66b9246b0843057aa37d8f0250badd2b351e5
DESKTOP-9TD4M53\HaiNT126 <HaiNT126>
2024-12-22 09:36:27 +0700

    no message

--- TemplateCore/templatecore_vuetify/src/components.d.ts
+++ TemplateCore/templatecore_vuetify/src/components.d.ts
@@ -10,8 +10,6 @@ declare module 'vue' {
     AppFooter: typeof import('./components/AppFooter.vue')['default']
     MainHome: typeof import('./components/Main/MainHome.vue')['default']
     MainPage: typeof import('./components/Main/MainPage.vue')['default']
-    MainPageHome: typeof import('./router/MainHome/MainHomePage.vue')['default']
-    NavagitionPage: typeof import('./router/MainHome/NavagitionPage.vue')['default']
     Navigation: typeof import('./components/Main/Navigation.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']

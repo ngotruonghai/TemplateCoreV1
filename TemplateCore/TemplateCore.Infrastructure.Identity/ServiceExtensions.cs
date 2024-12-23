867b302d064dda7b568163c540869d9346ef7182
haint0500 <ngotruonghai@vietbank.com.vn>
2024-12-23 11:29:53 +0700

    BE

--- TemplateCore/TemplateCore.Infrastructure.Identity/ServiceExtensions.cs
+++ TemplateCore/TemplateCore.Infrastructure.Identity/ServiceExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using TemplateCore.Application.Wrappers;
 using TemplateCore.Domain.Settings;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TemplateCore.Infrastructure.Identity
 {
@@ -36,53 +37,53 @@ namespace TemplateCore.Infrastructure.Identity
             //services.AddTransient<IAccountService, AccountService>();
             #endregion
             services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
-            
-            //services.AddAuthentication(options =>
-            //{
-            //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //})
-            //    .AddJwtBearer(o =>
-            //    {
-            //        o.RequireHttpsMetadata = false;
-            //        o.SaveToken = false;
-            //        o.TokenValidationParameters = new TokenValidationParameters
-            //        {
-            //            ValidateIssuerSigningKey = true,
-            //            ValidateIssuer = true,
-            //            ValidateAudience = true,
-            //            ValidateLifetime = true,
-            //            ClockSkew = TimeSpan.Zero,
-            //            ValidIssuer = configuration["JWTSettings:Issuer"],
-            //            ValidAudience = configuration["JWTSettings:Audience"],
-            //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
-            //        };
-            //        o.Events = new JwtBearerEvents()
-            //        {
-            //            OnAuthenticationFailed = c =>
-            //            {
-            //                c.NoResult();
-            //                c.Response.StatusCode = 500;
-            //                c.Response.ContentType = "text/plain";
-            //                return c.Response.WriteAsync(c.Exception.ToString());
-            //            },
-            //            OnChallenge = context =>
-            //            {
-            //                context.HandleResponse();
-            //                context.Response.StatusCode = 401;
-            //                context.Response.ContentType = "application/json";
-            //                var result = JsonConvert.SerializeObject(new Response<string>("You are not Authorized"));
-            //                return context.Response.WriteAsync(result);
-            //            },
-            //            OnForbidden = context =>
-            //            {
-            //                context.Response.StatusCode = 403;
-            //                context.Response.ContentType = "application/json";
-            //                var result = JsonConvert.SerializeObject(new Response<string>("You are not authorized to access this resource"));
-            //                return context.Response.WriteAsync(result);
-            //            },
-            //        };
-            //    });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(o =>
+                {
+                    o.RequireHttpsMetadata = false;
+                    o.SaveToken = false;
+                    o.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = configuration["JWTSettings:Issuer"],
+                        ValidAudience = configuration["JWTSettings:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
+                    };
+                    o.Events = new JwtBearerEvents()
+                    {
+                        OnAuthenticationFailed = c =>
+                        {
+                            c.NoResult();
+                            c.Response.StatusCode = 500;
+                            c.Response.ContentType = "text/plain";
+                            return c.Response.WriteAsync(c.Exception.ToString());
+                        },
+                        OnChallenge = context =>
+                        {
+                            context.HandleResponse();
+                            context.Response.StatusCode = 401;
+                            context.Response.ContentType = "application/json";
+                            var result = JsonConvert.SerializeObject(new Response<string>("You are not Authorized"));
+                            return context.Response.WriteAsync(result);
+                        },
+                        OnForbidden = context =>
+                        {
+                            context.Response.StatusCode = 403;
+                            context.Response.ContentType = "application/json";
+                            var result = JsonConvert.SerializeObject(new Response<string>("You are not authorized to access this resource"));
+                            return context.Response.WriteAsync(result);
+                        },
+                    };
+                });
         }
 
         public static void AddIdentityLayer(this IServiceCollection services)
--- TemplateCore/TemplateCore.Server/Program.cs
+++ TemplateCore/TemplateCore.Server/Program.cs
@@ -1,5 +1,6 @@
 
 using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
 using System.Reflection;
 using TemplateCore.Infrastructure.Identity;
 using TemplateCore.Infrastructure.Identity.Contexts;
@@ -30,6 +31,44 @@ namespace TemplateCore.Server
 
             _services.AddSwaggerGen(c =>
             {
+                //c.IncludeXmlComments(string.Format(@"TemplateCore.WebApp.Server.xml"));
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Clean Architecture - TemplateCore.WebApi",
+                    Description = "This Api will be responsible for overall data distribution and authorization.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "codewithmukesh",
+                        Email = "hello@codewithmukesh.com",
+                        Url = new Uri("https://codewithmukesh.com/contact"),
+                    }
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    Description = "Input your Bearer token in this format - Bearer {your token here} to access this API",
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                            Scheme = "Bearer",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        }, new List<string>()
+                    },
+                });
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
@@ -41,7 +80,6 @@ namespace TemplateCore.Server
             _services.AddSqlServerIdentityInfrastructure(typeof(Program).Assembly.FullName);
             _services.AddIdentityLayer();
             _services.AddIdentityRepositories(_config);
-
             #endregion
 
 
@@ -66,6 +104,7 @@ namespace TemplateCore.Server
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
+
 
 
             app.MapControllers();

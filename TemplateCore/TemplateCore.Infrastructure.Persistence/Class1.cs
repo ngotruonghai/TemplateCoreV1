f1ec14537b5ccd22b071c28ad8c0c26dcc0bf368
haint0500 <ngotruonghai@vietbank.com.vn>
2024-12-23 10:18:09 +0700

    dsfe

---
+++ TemplateCore/TemplateCore.Infrastructure.Persistence/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateCore.Infrastructure.Persistence
+{
+    internal class Class1
+    {
+    }
+}
